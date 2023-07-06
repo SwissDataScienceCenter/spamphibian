311192b9e703f13a2c902a2f28901053410ec5a6
Wes Johnson <wes@wes-johnson.com>
2023-07-06 17:07:49 +0200

    add event scenarios and add back to redis

--- event_service/app.py
+++ event_service/app.py
@@ -5,63 +5,69 @@ import redis
 import json
 from functools import partial
 
+
 def create_app(app_name: str) -> Sanic:
     app = Sanic(app_name)
 
     # Create Redis connection
-    redis_conn = redis.Redis(host='localhost', port=6379, db=0)
+    redis_conn = redis.Redis(host="localhost", port=6379, db=0)
 
-    @app.post('/event')
+    @app.post("/event")
     async def handle_event(request):
         gitlab_event = request.json
 
         # Determine the type of event
-        event_name = gitlab_event.get('event_name')
-        object_kind = gitlab_event.get('object_kind')
-        action = gitlab_event.get('object_attributes', {}).get('action')
+        event_name = gitlab_event.get("event_name")
+        object_kind = gitlab_event.get("object_kind")
+        action = gitlab_event.get("object_attributes", {}).get("action")
 
         # Project-related events
-        if event_name in ['project_create',
-                        'project_rename',
-                        'project_transfer',
-                        'user_create',
-                        'user_rename',
-                        'group_create',
-                        'group_rename',
-                        'snippet_check']:
+        if event_name in [
+            "project_create",
+            "project_rename",
+            "project_transfer",
+            "user_create",
+            "user_rename",
+            "group_create",
+            "group_rename",
+            "snippet_check",
+        ]:
             queue_name = event_name
 
         # Issue-related events
-        elif object_kind == 'issue' and action in ['open', 'close', 'reopen', 'update']:
-            queue_name = f'issue_{action}'
+        elif object_kind == "issue" and action in ["open", "close", "reopen", "update"]:
+            queue_name = f"issue_{action}"
 
         # Note-related events
-        elif object_kind == 'note':
+        elif object_kind == "note":
             try:
                 # Check if 'note' exists in 'object_attributes'
-                noteable_type = gitlab_event['object_attributes']['noteable_type']
+                noteable_type = gitlab_event["object_attributes"]["noteable_type"]
                 # Check if 'noteable_type' is 'Issue'
-                if noteable_type == 'Issue':
+                if noteable_type == "Issue":
                     # Check if 'created_at' and 'updated_at' are equal, meaning the note was just created
-                    if gitlab_event['object_attributes']['created_at'] == gitlab_event['object_attributes']['updated_at']:
-                        queue_name = f'issue_note_create'
+                    if (
+                        gitlab_event["object_attributes"]["created_at"]
+                        == gitlab_event["object_attributes"]["updated_at"]
+                    ):
+                        queue_name = f"issue_note_create"
                     else:
-                        queue_name = f'issue_note_update'
+                        queue_name = f"issue_note_update"
 
             except KeyError:
                 print("object_attributes.note does not exist in gitlab_event")
 
-        
         else:
-            print(f'Unhandled event: {event_name if event_name else object_kind}')
+            print(f"Unhandled event: {event_name if event_name else object_kind}")
 
         queue_name = "event_" + queue_name
-        
+
         redis_conn.lpush(queue_name, json.dumps(gitlab_event))
 
         return sanic_json({"message": "Event received"})
 
     return app
+
 
 if __name__ == "__main__":
     loader = AppLoader(factory=partial(create_app, "EventService"))
--- verification_service/app.py
+++ verification_service/app.py
@@ -3,48 +3,70 @@ import redis
 from time import sleep
 import yaml
 import re
+import requests
+
 
 def check_domain_verification(email):
     # Load verified domains from yaml file
-    with open('verification_service/verified_domains.yaml', 'r') as file:
-        verified_domains = yaml.safe_load(file)['domains']
+    with open("verification_service/verified_domains.yaml", "r") as file:
+        verified_domains = yaml.safe_load(file)["domains"]
 
     for domain in verified_domains:
         if re.search(domain, email):
             return True
     return False
 
+
 def get_user_email_address(event_type, event_data):
-    if event_type in ['project_create', 'project_rename', 'project_transfer']:
-        return event_data.get('owner_email')
+    if event_type in ["project_create", "project_rename", "project_transfer"]:
+        return event_data.get("owner_email")
 
-    elif event_type in ['user_create', 'user_rename']:
-        return event_data.get('email')
+    elif event_type in ["user_create", "user_rename"]:
+        return event_data.get("email")
 
-    elif event_type in ['issue_open', 'issue_update', 'issue_close', 'issue_reopen', 'issue_note_create', 'issue_note_update']:
-        user_attributes = event_data.get('user', {})
-        return user_attributes.get('email')
+    elif event_type in [
+        "issue_open",
+        "issue_update",
+        "issue_close",
+        "issue_reopen",
+        "issue_note_create",
+        "issue_note_update",
+    ]:
+        user_attributes = event_data.get("user", {})
+        return user_attributes.get("email")
 
     else:
         print(f"Unable to get user email address for this event type: {event_type}")
         return None
 
+
 def process_events():
     # Set up Redis connection
-    redis_conn = redis.Redis(host='localhost', port=6379, db=0)
+    redis_conn = redis.Redis(host="localhost", port=6379, db=0)
 
     # List of event types to listen for
-    event_types = ['project_create', 'project_rename', 'project_transfer',
-                   'issue_open', 'issue_close', 'issue_reopen', 'issue_update',
-                   'user_create', 'user_rename', 'group_create', 'group_rename',
-                   'snippet_check', 'issue_note_update', 'issue_note_create']
+    event_types = [
+        "project_create",
+        "project_rename",
+        "project_transfer",
+        "issue_open",
+        "issue_close",
+        "issue_reopen",
+        "issue_update",
+        "user_create",
+        "user_rename",
+        "group_create",
+        "group_rename",
+        "snippet_check",
+        "issue_note_update",
+        "issue_note_create",
+    ]
 
     while True:
         for event_type in event_types:
-
             # Pop the next event off the queue
             event = redis_conn.lpop("event_" + event_type)
-            
+
             # If there was no event, continue to the next event type
             if event is None:
                 continue
@@ -52,20 +74,53 @@ def process_events():
             # Parse the event data from JSON
             event_data = json.loads(event)
 
-            # Perform an action based on the event type
-            if event_type in ['project_create', 'project_rename', 'project_transfer',
-                              'user_create', 'user_rename', 'issue_open', 'issue_update',
-                              'issue_close', 'issue_reopen', 'issue_note_create', 'issue_note_update']:
+            user_email_address = None
 
+            # Perform an action based on the event type
+            if event_type in [
+                "project_create",
+                "project_rename",
+                "project_transfer",
+                "user_create",
+                "user_rename",
+                "issue_open",
+                "issue_update",
+                "issue_close",
+                "issue_reopen",
+                "issue_note_create",
+                "issue_note_update",
+            ]:
                 user_email_address = get_user_email_address(event_type, event_data)
 
-                if user_email_address is None:
-                    print(f"Unable to get user email address for this event type: {event_type}")
-                    
+            elif event_type in ["group_create", "group_rename"]:
+                print(
+                    f"{event_type} event type received, need to get user email from GitLab API"
+                )
 
+                group_id = event_data.get("id")
+
+                response = requests.get(
+                    f"https://gitlab.example.com/api/v4/groups/{group_id}/members",
+                    headers={"PRIVATE-TOKEN": "<your_access_token>"},
+                )
+                group_members = response.json()
+
+                for member in group_members:
+                    if member.get("access_level") == 50:
+                        user_email_address = member.get("email")
+                        break
+
+            ## add snippet_check event type
+
+            if user_email_address is None:
+                print(
+                    f"Unable to get user email address for this event type: {event_type}"
+                )
+            else:
                 domain_verified = check_domain_verification(user_email_address)
-
-                print(f"User email address {user_email_address} verification status: {domain_verified}")
+                print(
+                    f"User email address {user_email_address} verification status: {domain_verified}"
+                )
 
                 if domain_verified:
                     continue
@@ -73,12 +128,9 @@ def process_events():
                 queue_name = "verification_" + event_type
                 redis_conn.lpush(queue_name, json.dumps(event_data))
 
-            else:
-                print(f"{event_type} event type received, need to get user email from GitLab API")
-                # todo: get owner email address from gitlab api and check domain verification status
-
         # Sleep for a bit before checking the queues again
         sleep(1)
+
 
 if __name__ == "__main__":
     process_events()
