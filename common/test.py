7fa05833b280b054eba00fc9ce875af98709c170
Wes Johnson <wes@wes-johnson.com>
2024-01-19 15:06:48 +0100

    fix eventprocessor tests

--- common/test.py
+++ common/test.py
@@ -1,27 +1,38 @@
 import unittest
 from common.event_processor import EventProcessor
-from test.mock_redis import MockRedis
+import fakeredis
 import json
 
 class TestEventProcessor(unittest.TestCase):
 
     def setUp(self):
-        self.redis_conn = MockRedis()
-        self.event_processor = EventProcessor("retrieval", ["user_create"], self.redis_conn)
+        self.redis_conn = fakeredis.FakeRedis()
+        self.event_processor = EventProcessor("retrieval", "classification", self.redis_conn)
     
     def test_initialization_with_redis_conn(self):
         self.assertEqual(self.event_processor.redis_client, self.redis_conn)
 
     def test_poll_and_process_event(self):
         event_data = {"username": "test_user", "some_data": "data"}
-        self.redis_conn.lpush("retrieval_user_create", json.dumps(event_data))
+        self.redis_conn.xadd("retrieval", {"user_create": json.dumps(event_data)})
     
-    def test_push_event_to_queue(self):
+    def test_add_event_to_queue(self):
         event_data = {"test_key": "test_value"}
-        self.event_processor.push_event_to_queue("user_create", event_data, "retrieval")
-        
-        popped_data = json.loads(self.redis_conn.lpop("retrieval_user_create").decode("utf-8"))
-        self.assertEqual(popped_data, event_data)
+        self.event_processor.push_event_to_queue("user_create", event_data)
+
+        while True:
+            messages = self.redis_conn.xread({self.event_processor.output_stream_name: '0'}, block=10000, count=1)
+            if messages:
+                for message in messages[0][1]:
+                    message_id = message[0]
+                    for key in message[1].keys():
+                        popped_data = json.loads(message[1][key].decode('utf-8'))
+
+                        self.redis_conn.xdel(self.event_processor.output_stream_name, message_id)
+
+                        self.assertEqual(popped_data, event_data)
+
+                return
 
     def test_process_event_raises_error(self):
         with self.assertRaises(NotImplementedError):
